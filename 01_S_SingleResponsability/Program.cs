ba8f793f89d5669a73638dfd4ec80e93e115ea55
Kuester <marcoskuester@gmail.com>
2020-09-12 16:16:57 -0300

    Add Interface Segregation

--- 01_S_SingleResponsability/Program.cs
+++ 01_S_SingleResponsability/Program.cs
@@ -5,44 +5,44 @@ using static System.Console;
 
 namespace _01_S_SingleResponsability
 {
+    public class Alarm
+    {
+        private List<DateTime> _alarmList = new List<DateTime>();
+
+        public Alarm()
+        { }
+
+        public void AddAlarm(DateTime alarm)
+        {
+            _alarmList.Add(alarm);
+        }
+
+        public override string ToString()
+        {
+            string alarmText = "";
+            foreach (var alarm in _alarmList)
+            {
+                alarmText += alarm + "\r\n";
+            }
+            return alarmText;
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToDisk(string fileName, Alarm alarm)
+        {
+            File.WriteAllText(fileName, alarm.ToString());
+        }
+
+        public void ReadFromDisk(string fileName)
+        {
+            Write(File.ReadAllText(fileName));
+        }
+    }
+
     class Program
     {
-        public class Alarm
-        {
-            private List<DateTime> _alarmList = new List<DateTime>();
-
-            public Alarm()
-            { }
-
-            public void AddAlarm(DateTime alarm)
-            {
-                _alarmList.Add(alarm);
-            }
-
-            public string GetAlarmsToString()
-            {
-                string alarmText = "";
-                foreach (var alarm in _alarmList)
-                {
-                    alarmText += alarm + "\r\n";
-                }
-                return alarmText;
-            }
-        }
-
-        public class Persistence
-        {
-            public void SaveToDisk(string fileName, Alarm alarm)
-            {                
-                File.WriteAllText(fileName, alarm.GetAlarmsToString());
-            }
-
-            public void ReadFromDisk(string fileName)
-            {
-                Write(File.ReadAllText(fileName));
-            }
-        }
-
         static void Main(string[] args)
         {
             var alarms = new Alarm();            
---
+++ 04_I_InterfaceSegregation/Program.cs
@@ -0,0 +1,88 @@
+﻿using static System.Console;
+
+namespace _04_I_InterfaceSegregation
+{
+    public class Document
+    {
+        public string Text { get; set; }
+
+        public Document(string text)
+        {
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"{Text}";
+        }
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IPhotoCopier : IScanner, IPrinter
+    {
+        void Copy(Document d);
+    }
+
+    public class SimplePrinter : IPrinter
+    {
+        public void Print(Document d)
+        {
+            WriteLine($"Print {d}");
+        }
+    }
+
+    public class SimpleScanner : IScanner
+    {
+        public void Scan(Document d)
+        {
+            WriteLine($"Scan {d}");
+        }
+    }
+
+    public class MultiFunctionalDevice : IPhotoCopier
+    {
+        public void Copy(Document d)
+        {
+            WriteLine($"Copy {d}");
+        }
+
+        public void Print(Document d)
+        {
+            WriteLine($"Print {d}");
+        }
+
+        public void Scan(Document d)
+        {
+            WriteLine($"Scan {d}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txt = @"This is a document"; ;
+            var doc = new Document(txt);
+
+            var printer = new SimplePrinter();
+            printer.Print(doc);
+
+            var scanner = new SimpleScanner();
+            scanner.Scan(doc);
+
+            var multi = new MultiFunctionalDevice();
+            multi.Copy(doc);
+            multi.Scan(doc);
+            multi.Print(doc);
+        }
+    }
+}
