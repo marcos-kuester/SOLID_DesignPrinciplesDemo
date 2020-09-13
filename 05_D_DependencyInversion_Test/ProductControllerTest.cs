fd2425cdc88509e5731f1862d0e1195636d687d0
Kuester <marcoskuester@gmail.com>
2020-09-13 16:02:20 -0300

    Add Mock to Unit Test

--- 05_D_DependencyInversion_Test/ProductControllerTest.cs
+++ 05_D_DependencyInversion_Test/ProductControllerTest.cs
@@ -1,5 +1,6 @@
 using _05_D_DependencyInversion;
-using System;
+using Moq;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -13,13 +14,30 @@ namespace _05_D_DependencyInversion_Test
             // arrange
             var loger = new AppLogger();
             var productRepo = new ProductRepository();
-            var productController = new ProductController(loger, productRepo);            
+            var productController = new ProductController(loger, productRepo);                       
 
             // act
             var result = productController.GetAll();
 
             // assert
             Assert.Equal(3, result.Count());
+        }
+
+        [Fact]
+        public void Product_Should_Return_Success()
+        {
+            // arrange
+            var loger = new AppLogger();
+            var mockProductRepo = new Mock<IProductRepository>();
+            mockProductRepo.Setup(x => x.GetAll())
+                .Returns(new List<Product>() { new Product { Id = 1, Name = "PC", Price = 1000 } });
+            var productController = new ProductController(loger, mockProductRepo.Object);
+
+            // act
+            var result = productController.GetAll();
+
+            // assert
+            Assert.Equal(1000, result.First().Price);
         }
     }
 }
